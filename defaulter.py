ff375c75865655fe1a02497bffb30c5d1f215b81
Software Engineer <engineer@abc-software.com>
2026-07-21 12:17:32 +0530

    Added defaulter list feature

---
+++ defaulter.py
@@ -0,0 +1,24 @@
+"""
+Defaulter List Module
+Responsibilities:
+- Generate defaulter list
+- Identify students with low attendance
+"""
+
+def get_defaulter_list(minimum_percentage=75):
+    """Generate list of defaulters based on attendance percentage"""
+    defaulters = []
+    # Get students with attendance below minimum percentage
+    return defaulters
+
+def send_defaulter_notification():
+    """Send notification to defaulter students"""
+    pass
+
+def generate_defaulter_report():
+    """Generate detailed defaulter report"""
+    pass
+
+def mark_defaulter_status(student_id, is_defaulter):
+    """Mark or unmark a student as defaulter"""
+    pass
